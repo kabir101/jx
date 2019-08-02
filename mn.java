09699352a42bae76fb6a993697f833cff8f4a7a3
kabir101 <53597751+kabir101@users.noreply.github.com>
2019-08-02 13:08:37 +0600

    Add files via upload

---
+++ mn.java
@@ -0,0 +1,17 @@
+package trs;
+import java.util.*;
+class X{
+	int a,b;
+	X(int x,int y){
+		a=x;
+		b=y;
+	}
+}
+public class mn {
+
+	public static void main(String[] args) {
+		X p=new X(2,3);
+		System.out.println(p.a+" "+p.b);
+	}
+
+}
